20281fedf933e2c430f2dbcf5f9334f5add36791
jackspirou <jack.spirou@me.com>
2024-09-15 00:11:16 -0500

    add links to README.md

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package publicid
 
-//go:generate gomarkdoc -o README.md -e .
+//go:generate gomarkdoc -o README.md -e . --repository.url https://github.com/addmax-ai/publicid --repository.default-branch master --repository.path /
